31f3bfdec1cd47f2a2620040ecbbaa42da23af39
Edvin Sladic <sladic.edvin@gmail.com>
2018-07-13 21:16:43 +0200

    First commit

---
+++ app/src/main/java/se/sladic/openlauncher/AppInfo.java
@@ -0,0 +1,9 @@
+package se.sladic.openlauncher;
+
+import android.graphics.drawable.Drawable;
+
+public class AppInfo {
+    CharSequence label;
+    CharSequence packageName;
+    Drawable icon;
+}
---
+++ app/src/main/java/se/sladic/openlauncher/DrawerActivity.java
@@ -0,0 +1,13 @@
+package se.sladic.openlauncher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrawerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drawer);
+    }
+}
